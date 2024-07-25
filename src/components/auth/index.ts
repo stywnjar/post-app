bf6fe51d87d3cccbef9c5deabd9a4114f6408522
stywn04 <stywn.jar@gmail.com>
2024-07-26 03:53:06 +0700

    feat: implement react-hook-form + zod for validation

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,3 +1,5 @@
 export { AuthHeader } from "./header";
 export { AuthFooter } from "./footer";
 export { LoginForm } from "./login-form";
+export { RegisterForm } from './register-form'
+export { FieldError } from './field-error'
---
+++ src/schema/auth.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Password too short, min 8 chars!" }),
+});
+
+export type loginType = z.infer<typeof loginSchema>;
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 chars!" }),
+  password: z.string().min(8, { message: "Password too short, min 8 chars!" }),
+});
+
+export type registerType = z.infer<typeof registerSchema>;
